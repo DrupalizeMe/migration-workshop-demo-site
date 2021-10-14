ec32d5b094f1d54b86082a444c905733065f1d71
Joe Shindelar <eojthebrave@gmail.com>
2021-10-14 10:37:47 -0500

    Reset script should delete config and clear cache.

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Deleting all artifacts from drush migrate:upgrade --configure-only"
+
+drush sqlq 'delete from config where name like "migrate_plus.migration.%"'
 drush php:eval "\Drupal::state()->set('source_db', ['key' => 'migrate','target' => 'default',]);"
 drush php:eval "\Drupal::state()->set('migrate.fallback_state_key', 'source_db');"
+drush cr -y
