a69224ab485d21260c6db368ab68f55338fecac8
Joe Shindelar <eojthebrave@gmail.com>
2021-10-15 10:58:53 -0500

    More fixes to ensure everything runs smoothly on gitpod init.

--- .gitpod/preview-7.sh
+++ .gitpod/preview-7.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo "Drupal 7 site is available at: $(gp url 8080)/_d7web/"
 gp preview "$(gp url 8080)/_d7web/"
--- .gitpod/preview.sh
+++ .gitpod/preview.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo "Drupal 9 site is available at: $(gp url 8080)"
 gp preview "$(gp url 8080)"
