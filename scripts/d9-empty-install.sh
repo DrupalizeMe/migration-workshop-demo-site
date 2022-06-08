c8784b601229a7cd50c94c28371f3c61d215c651
Joe Shindelar <eojthebrave@gmail.com>
2022-06-08 10:32:57 -0500

    Update paragraphs module now that stable release includes migration fixes. Remove uncessary escaping from startup script.

--- scripts/d9-empty-install.sh
+++ scripts/d9-empty-install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -ex
+
 # Install Drupal and dependencies.
 ddev composer install
 
 echo "Installing Drupal core"
-ddev drush si --yes --site-name=\"Drupalize.Me Migration Workshop Demo Site\" --account-pass=\"admin\"
+ddev drush si --yes --site-name="Drupalize.Me Migration Workshop Demo Site" --account-pass="admin" standard
 
 echo "Installing contributed modules"
 ddev drush en --yes address admin_toolbar honeypot image_resize_filter migrate_tools migrate_plus migrate_upgrade paragraphs pathauto recaptcha token webform
