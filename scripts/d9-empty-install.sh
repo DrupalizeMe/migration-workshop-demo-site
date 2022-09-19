37fec21c4d4e44ddc1a7c16b6793779db3c370fc
Joe Shindelar <eojthebrave@gmail.com>
2022-09-19 11:39:55 -0500

    --site-name not working on gitpod at the moment.

--- scripts/d9-empty-install.sh
+++ scripts/d9-empty-install.sh
@@ -6,7 +6,7 @@ set -ex
 ddev composer install
 
 echo "Installing Drupal core"
-ddev drush si --yes --site-name="Drupalize.Me Migration Workshop Demo Site" --account-pass="admin" standard
+ddev drush si --yes --account-pass="admin" standard
 
 echo "Installing contributed modules"
 ddev drush en --yes address admin_toolbar honeypot image_resize_filter migrate_tools migrate_plus migrate_upgrade paragraphs pathauto recaptcha token webform
