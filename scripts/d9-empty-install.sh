f60d8c9ba0404beb462d216ef9d770ca9f8b1463
Joe Shindelar <eojthebrave@gmail.com>
2023-10-09 14:38:45 -0500

    Upgrade to Drupal 10.

--- scripts/d9-empty-install.sh
+++ scripts/d9-empty-install.sh
@@ -9,4 +9,4 @@ echo "Installing Drupal core"
 ddev drush si --yes --account-pass="admin" standard
 
 echo "Installing contributed modules"
-ddev drush en --yes address admin_toolbar honeypot image_resize_filter migrate_tools migrate_plus migrate_upgrade paragraphs pathauto recaptcha token webform
+ddev drush en --yes address admin_toolbar honeypot migrate_tools migrate_plus migrate_upgrade paragraphs pathauto recaptcha token webform
