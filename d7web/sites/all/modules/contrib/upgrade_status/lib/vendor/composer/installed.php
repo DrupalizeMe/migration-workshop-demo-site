e84f24653ba52b1f25929ca87b2d2cace0382a29
Joe Shindelar <eojthebrave@gmail.com>
2023-11-06 11:17:44 -0600

    Update upgrade_status on D7 site.

---
+++ d7web/sites/all/modules/contrib/upgrade_status/lib/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'drupal_upgrade_status/lib',
+        'pretty_version' => 'dev-7.x-1.x',
+        'version' => 'dev-7.x-1.x',
+        'reference' => 'fd69a5ba28a603a64ec019e61577e4500368da05',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/semver' => array(
+            'pretty_version' => '3.3.2',
+            'version' => '3.3.2.0',
+            'reference' => '3953f23262f2bff1919fc82183ad9acb13ff62c9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/./semver',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'drupal_upgrade_status/lib' => array(
+            'pretty_version' => 'dev-7.x-1.x',
+            'version' => 'dev-7.x-1.x',
+            'reference' => 'fd69a5ba28a603a64ec019e61577e4500368da05',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
